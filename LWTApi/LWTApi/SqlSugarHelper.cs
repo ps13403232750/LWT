50d11e4b33a3283008bb62a2a24e194462f4500e
zhangchenxi <18337853917@163.com>
2018-11-06 10:52:36 +0800

    LWT

---
+++ LWTApi/LWTApi/SqlSugarHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SqlSugar;
+namespace LWTApi
+{
+    public class SqlSugarHelper
+    {
+        public static SqlSugarClient GetInstance()
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig() { ConnectionString = Config.ConnectionString, DbType = DbType.Oracle, IsAutoCloseConnection = true });
+            db.Ado.IsEnableLogEvent = true;
+            db.Ado.LogEventStarting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" + db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                Console.WriteLine();
+            };
+            return db;
+        }
+    }
+}
