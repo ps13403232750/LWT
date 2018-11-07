deacb3688cb1ca246df77a94b565f1ec77555364
zhangchenxi <18337853917@163.com>
2018-11-07 14:25:12 +0800

    LWT

---
+++ LWTApi/IServices/ISupplierServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Model;
+namespace IServices
+{
+    public interface ISupplierServices
+    {
+        /// <summary>
+        /// 获取商品列表
+        /// </summary>
+        /// <returns></returns>
+        List<Goods> GetGoods();
+    }
+}
---
+++ LWTApi/Services/SupplierServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Model;
+using IServices;
+using SqlSugar;
+namespace Services
+{
+   public class SupplierServices: BaseDB,ISupplierServices
+    {
+        SimpleClient<Goods> goods = new SimpleClient<Goods>(GetInstance());
+
+        /// <summary>
+        /// 获取商品表数据
+        /// </summary>
+        /// <returns></returns>
+        public List<Goods> GetGoods() => goods.GetList();
+    }
+}
