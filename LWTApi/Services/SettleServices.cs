998c3a5f42daa781ed6e70761dce9c9e12a167b7
13130 <44598278+13130@users.noreply.github.com>
2018-11-07 13:23:47 +0800

    LWT

--- LWTApi/Services/SettleServices.cs
+++ LWTApi/Services/SettleServices.cs
@@ -7,7 +7,7 @@ using Model;
 using IServices;
 namespace Services
 {
-   public  class SettleServices: BaseDB,ISettleServices
+    public class SettleServices : BaseDB, ISettleServices
     {
         SimpleClient<ManageLimit> ML = new SimpleClient<ManageLimit>(GetInstance());
         SimpleClient<PurChaseNumber> CN = new SimpleClient<PurChaseNumber>(GetInstance());
@@ -27,7 +27,7 @@ namespace Services
         /// 结算次数
         /// </summary>
         /// <returns></returns>
-        public List<PurChaseNumber>GetPurChaseNumber()
+        public List<PurChaseNumber> GetPurChaseNumber()
         {
             return CN.GetList();
         }
