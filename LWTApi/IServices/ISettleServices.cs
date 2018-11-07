61a46a070bfbc16d953926ba7fffcc203e39a8fb
13130 <44598278+13130@users.noreply.github.com>
2018-11-07 11:24:11 +0800

    LWT

---
+++ LWTApi/IServices/ISettleServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Model;
+namespace IServices
+{
+   public  interface ISettleServices
+    {
+        /// <summary>
+        /// 额度管理表
+        /// </summary>
+        /// <returns></returns>
+        List<ManageLimit> GetManageLimits();
+        /// <summary>
+        /// 结算次数
+        /// </summary>
+        /// <returns></returns>
+        List<PurChaseNumber> GetPurChaseNumber();
+    }
+}
---
+++ LWTApi/Services/SettleServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SqlSugar;
+using Model;
+using IServices;
+namespace Services
+{
+   public  class SettleServices: BaseDB,ISettleServices
+    {
+        SimpleClient<ManageLimit> ML = new SimpleClient<ManageLimit>(GetInstance());
+        SimpleClient<PurChaseNumber> CN = new SimpleClient<PurChaseNumber>(GetInstance());
+        /// <summary>
+        /// 额度表显示
+        /// </summary>
+        /// <returns></returns>
+        public List<ManageLimit> GetManageLimits()
+        {
+            return ML.GetList();
+        }
+        /// <summary>
+        /// 结算次数
+        /// </summary>
+        /// <returns></returns>
+        public List<PurChaseNumber>GetPurChaseNumber()
+        {
+            return CN.GetList();
+        }
+    }
+}
