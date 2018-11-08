ae98b67c9238816cdf555f878741fadd2145d5a2
ps13403232750 <136187537@qq.com>
2018-11-08 19:07:23 +0800

    LWT

--- LWT/LWT.Client/Controllers/IndexController.cs
+++ LWT/LWT.Client/Controllers/IndexController.cs
@@ -29,16 +29,13 @@ namespace LWT.Client.Controllers
         /// <returns></returns>
         public IActionResult Add()
         {
-            GetRoles();
             return View();
         }
-        
-        public int Adddo(string userName)
+        [HttpPost]
+        public int Add(Users user)
         {
-            //var data = Common.Client.GetApi("Post","Values/Add");
-            //return int.Parse(data);
-
-            return 1;
+            var data = Common.Client.GetApi("Post","Values/Add");
+            return int.Parse(data);
         }
 
         /// <summary>
@@ -65,14 +62,7 @@ namespace LWT.Client.Controllers
         {
             string sql = Common.Client.GetApi("Get", "Values/GetRoles");
             return sql;
-            //var data = from s in list
-            //           select new SelectListItem
-            //           {
-            //               Text = s.RoleName,
-            //               Value = s.Id.ToString()
-            //           };
-            //ViewBag.roles = data.ToList();
-                       
+           
         }
 
 
