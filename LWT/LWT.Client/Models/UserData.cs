49d21d0e89e7d10bd819731df4ab3bb5e0ec41e4
ps13403232750 <136187537@qq.com>
2018-11-08 08:02:12 +0800

    LWT

---
+++ LWT/LWT.Client/Models/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using LWT.Model;
+
+namespace LWT.Client.Models
+{
+    public class UserData
+    {
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 用户角色
+        /// </summary>
+        public Roles Role { get; set; }
+
+    }
+}
