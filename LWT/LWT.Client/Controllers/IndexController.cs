99fc00e9fc51ef935b08c4686b4edba39a42ad58
691234097 <44598858+691234097@users.noreply.github.com>
2018-11-08 16:25:04 +0800

    LWT

--- LWT/LWT.Client/Controllers/IndexController.cs
+++ LWT/LWT.Client/Controllers/IndexController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using LWT.Common;
 using LWT.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LWT.Client.Controllers
 {
@@ -21,5 +22,74 @@ namespace LWT.Client.Controllers
             string getpower = Common.Client.GetApi("get","Values/GetPower");
             return View(JsonConvert.DeserializeObject<List<Power>>(getpower));
         }
+
+        /// <summary>
+        /// 注册用户
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            GetRoles();
+            return View();
+        }
+        [HttpPost]
+        public int Add(string UserName)
+        {
+            var data = Common.Client.GetApi("Post","Values/Add");
+            return int.Parse(data);
+        }
+
+        /// <summary>
+        /// 根据角色添加权限
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AddRole()
+        {
+            string getpower = Common.Client.GetApi("get", "Values/GetPower");
+            return View(JsonConvert.DeserializeObject<List<Power>>(getpower));
+        }
+        [HttpPost]
+        public int AddRole(RoleAndPower roleAndPower)
+        {
+            var data = Common.Client.GetApi("post", "Values/AddRoleAndPower");
+            return int.Parse(data);
+        }
+
+        #region 所有下拉菜单
+        /// <summary>
+        /// 获取角色名称的下拉菜单
+        /// </summary>
+        public string GetRoles()
+        {
+            string sql = Common.Client.GetApi("Get", "Values/GetRoles");
+            return sql;
+            //var data = from s in list
+            //           select new SelectListItem
+            //           {
+            //               Text = s.RoleName,
+            //               Value = s.Id.ToString()
+            //           };
+            //ViewBag.roles = data.ToList();
+                       
+        }
+
+
+        /// <summary>
+        /// 获取品牌名称的下拉菜单
+        /// </summary>
+        public void GetBrand()
+        {
+            string sql = Common.Client.GetApi("Get", "Values/GetBrand");
+            List<Brand> list = JsonConvert.DeserializeObject<List<Brand>>(sql);
+            var data = from s in list
+                       select new SelectListItem
+                       {
+                           Text = s.BrandName,
+                           Value = s.Id.ToString()
+                       };
+            ViewBag.brand = data.ToList();
+
+        }
+        #endregion
     }
 }
