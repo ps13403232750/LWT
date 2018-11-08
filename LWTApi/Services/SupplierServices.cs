29755ca25c4e038ac7951ced5ae3a25cfd4cd407
zhangchenxi <18337853917@163.com>
2018-11-08 16:26:02 +0800

    LWT

--- LWTApi/Services/SupplierServices.cs
+++ LWTApi/Services/SupplierServices.cs
@@ -26,13 +26,11 @@ namespace Services
         /// <returns></returns>
         public bool UpdateGoods(int Id, int State)
         {
-<<<<<<< HEAD
-            var result = GoodsDb.Update(it => new Goods() { State = State }, it => it.Id == Id);
-            return result;
-=======
+            //别删，我留着还有用，谢谢
+            //var result = GoodsDb.Update(it => new Goods() { State = State }, it => it.Id == Id);
+            //return result;
             var result = GetSimpleInstance<Goods>().Update(it => new Goods() { State = State }, it => it.Id == Id);
-            return Convert.ToInt32(result);
->>>>>>> d22afad3913962c21401aa4cd4e455468a61952f
+            return result;
         }
     }
 }
