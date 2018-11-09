22956adcd3e39ad0ae1f3d1f081a13d279000b7b
13130 <44598278+13130@users.noreply.github.com>
2018-11-09 13:16:01 +0800

    LWT

---
+++ LWTApi/Model/Orders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+  public class Orders
+    {
+        /// <summary>
+        /// 订单id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        public string OrderNum { get; set; }
+
+        /// <summary>
+        /// 订货日期
+        /// </summary>
+        public string OrderTime { get; set; }
+
+        /// <summary>
+        /// 订单总价
+        /// </summary>
+        public int Price { get; set; }
+
+        /// <summary>
+        /// 收货地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 采购商Id
+        /// </summary>
+        public int UserId { get; set; }
+    }
+}
